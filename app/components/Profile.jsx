6251111711bf3db4573cf75b507f20e17d2a7aaa
Ron Hamenahem <brklyn8900@gmail.com>
2016-01-05 22:47:28 -0500

    Add styling and containers

--- app/components/Profile.jsx
+++ app/components/Profile.jsx
@@ -1,25 +1,31 @@
 var React = require('react');
 var Router = require('react-router');
+var Repos = require('./Github/Repos.jsx');
+var UserProfile = require('./Github/userProfile.jsx');
+var Notes = require('./Notes/Notes.jsx');
 
 var Profile = React.createClass({
     getInitialState: function() {
       return {
-        notes: [],
-        bio: {},
-        repos: []
+        notes: [1,2,3],
+        bio: {
+          name: 'Ron Hamenahem'
+        },
+        repos: ["a","b","c"]
       }
     },
     render: function()  {
+      console.log(this.props)
       return(
         <div className="row">
           <div className="col-md-4">
-            User Profile Component --> {this.props.params.username}
+            <UserProfile username={this.props.params.username} bio={this.state.bio} />
           </div>
           <div className="col-md-4">
-            Repos Component
+            <Repos repos={this.state.repos}/>
           </div>
           <div className="col-md-4">
-            Notes Component
+            <Notes notes={this.state.notes}/>
           </div>
         </div>
       )
